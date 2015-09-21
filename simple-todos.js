ad655077c1bf3785ffe7d8c873401313d8f9bbb3
Michał Grygierzec <mgrygierzec@infusion.com>
2015-09-21 19:00:24 +0200

    Step 2

--- simple-todos.js
+++ simple-todos.js
@@ -1,19 +1,17 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // This code only runs on the client
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Template.body.helpers({
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    tasks: [
+
+      { text: "This is task 1" },
+
+      { text: "This is task 2" },
+
+      { text: "This is task 3" }
+
+    ]});
 }
 
 if (Meteor.isServer) {
