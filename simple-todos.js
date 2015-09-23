0207242ad29f23d49f01131cc6ff7813496fe9a4
Michał Grygierzec <michal.grygierzec@gmail.com>
2015-09-23 21:38:01 +0200

    Step 4

--- simple-todos.js
+++ simple-todos.js
@@ -6,8 +6,22 @@ if (Meteor.isClient) {
   Template.body.helpers({
 
     tasks: function() {
-      return Tasks.find({});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
+  });
+  
+  Template.body.events({
+
+    "submit .new-task": function (event) {
+      event.preventDefault();
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+      event.target.text.value = "";
+    }
+
   });
 }
 
