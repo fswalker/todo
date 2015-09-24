ec89f7ee462c257d7107d0646882477820dca32d
Michał Grygierzec <michal.grygierzec@gmail.com>
2015-09-24 23:58:37 +0200

    Step 5

--- simple-todos.js
+++ simple-todos.js
@@ -20,6 +20,24 @@ if (Meteor.isClient) {
         createdAt: new Date() // current time
       });
       event.target.text.value = "";
+    },
+    
+    "click .toggle-checked": function () {
+
+      // Set the checked property to the opposite of its current value
+
+      Tasks.update(this._id, {
+
+        $set: {checked: ! this.checked}
+
+      });
+
+    },
+
+    "click .delete": function () {
+
+      Tasks.remove(this._id);
+
     }
 
   });
