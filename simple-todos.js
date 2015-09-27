46db51df066bfd1474f4c902d1d0df66fedc0120
Michał Grygierzec <michal.grygierzec@gmail.com>
2015-09-27 15:07:30 +0200

    Step 9

--- simple-todos.js
+++ simple-todos.js
@@ -41,7 +41,9 @@ if (Meteor.isClient) {
       var text = event.target.text.value;
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
       event.target.text.value = "";
     },
@@ -68,6 +70,12 @@ if (Meteor.isClient) {
       Tasks.remove(this._id);
 
     }
+
+  });
+  
+  Accounts.ui.config({
+
+    passwordSignupFields: "USERNAME_ONLY"
 
   });
 }
